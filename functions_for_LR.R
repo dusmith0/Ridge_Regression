6a330e9992abb677689eb6daa291fa23c97e8660
Dustin Smith <dusmith0@tamu.edu>
2023-10-11 22:45:19 -0500

    I added a templete to help me work through the algorithm. This one is quite difficult. I spent 3 days just reading over it.

---
+++ functions_for_LR.R
@@ -0,0 +1,17 @@
+#Functions that might be useful 
+
+##Appatently this is called a softmax probability
+soft <- function(z){
+  exp(z)/sum(exp(z))
+}
+
+##To calculate the Objective Function
+
+
+##To calculate the Hessian-second matrix derivative
+
+
+##To calcualte the gradiant-first matrix derivative
+
+
+##To updateded betas
